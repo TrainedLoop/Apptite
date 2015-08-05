5a7303dc96bd71cd2f8f68857ef91e3f1b2d2567
Daniel Porto <ddanielporto@gmail.com>
2015-08-05 16:36:42 -0300

    First Commit

---
+++ Apptite.Domain/Builders/UserBuilder.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apptite.Domain.Builder
+{
+    public class UserBuilder
+    {
+
+        private string Name;
+        private string SurName;
+        private string Login;
+        private string Email;
+        private string Password;
+        private string FacebookId;
+
+
+        public UserBuilder WithName(string name)
+        {
+            if (name.Trim().Length < 2)
+                throw new ArgumentException("Nome Invalido Minimo 2 caracteres");
+            if (name.Trim().Length > 20)
+                throw new ArgumentException("Nome Invalido Maximo 20 caracteres");
+            Name = name;
+            return this;
+        }
+        public UserBuilder WithSurname(string surname)
+        {
+            if (surname.Trim().Length < 2)
+                throw new ArgumentException("Sobrenome Invalido Minimo 2 caracteres");
+            if (surname.Trim().Length > 20)
+                throw new ArgumentException("Sobrenome Invalido Maximo 20 caracteres");
+            SurName = surname;
+            return this;
+        }
+
+        public UserBuilder SetLogin(string login)
+        {
+            if (login.Trim().Length < 4)
+                throw new ArgumentException("Login Invalido Minimo 4 caracteres");
+            if (login.Trim().Length > 20)
+                throw new ArgumentException("Login Invalido Maximo 20 caracteres");
+            Login = login;
+            return this;
+        }
+
+        public UserBuilder SetPassword(string password, string passwordConfirmation)
+        {
+            if (password != passwordConfirmation)
+                throw new ArgumentException("Senhas não conferem");
+
+#warning  inserir validacao de letra e numero???
+            Password = password;
+            return this;
+        }
+
+        public UserBuilder SetEmail(string email)
+        {
+#warning  inserir validacao
+            Email = email;
+            return this;
+        }
+
+        public UserBuilder SetFacebookId(string facebookId)
+        {
+#warning  inserir validacao
+            FacebookId = facebookId;
+            return this;
+        }
+
+        public static implicit operator User(UserBuilder ub)
+        {
+
+            if (string.IsNullOrWhiteSpace(ub.Name))
+                throw new ArgumentNullException("O Nome está vazio");
+            if (string.IsNullOrWhiteSpace(ub.SurName))
+                throw new ArgumentNullException("O Sobrenome está vazio");
+            if (string.IsNullOrWhiteSpace(ub.Login))
+                throw new ArgumentNullException("O Login está vazio");
+            if (string.IsNullOrWhiteSpace(ub.Password))
+                throw new ArgumentNullException("A senha está vazia");
+
+
+            return new User(ub.Name, ub.SurName, ub.Login,ub.Email, ub.Password, ub.FacebookId);
+
+        }
+    }
+}
---
+++ Apptite.Domain/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apptite.Domain
+{
+    public class User
+    {
+        #region --//Properties//--
+        public virtual string Name { get; protected set; }
+        public virtual string SurName { get; protected set; }
+        public string FullName => Name + " " + SurName;
+
+        public virtual string Login { get; protected set; }
+        public virtual string Email { get; protected set; }
+        public virtual string Password { get; protected set; }
+        public virtual string FacebookId { get; protected set; }
+        // public virtual string GoogleId { get; private set; }  Hora de implementar?
+
+        #endregion
+
+        internal User(string name, string surName, string login, string email, string password, string facebookId = null)
+        {
+            Name = name;
+            SurName = surName;
+            Login = login;
+            Email = email;
+            Password = password;
+            FacebookId = facebookId;
+        }
+    }
+}
+
